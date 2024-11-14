6beeca2ac60260c8a8198e18f1375a37673decaa
gxnie <wlsdl7520@naver.com>
2024-11-15 03:32:03 +0900

    feat: Schedule 필드
    username, title, createdAt, updateAt필드 생성
    Column 설정

--- src/main/java/com/example/schedules_develop/entity/Schedule.java
+++ src/main/java/com/example/schedules_develop/entity/Schedule.java
@@ -1,7 +1,10 @@
 package com.example.schedules_develop.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
 @Entity
 @Table(name = "schedule")
 public class Schedule extends BaseEntity {
@@ -11,11 +14,24 @@ public class Schedule extends BaseEntity {
 
     @Column(columnDefinition = "longtext")
     // columnDefinition = "longtext"**는 해당 컬럼의 데이터 타입을 LONGTEXT로 지정한다는 의미
-    private String content;
+    private String contents;
 
+    @Setter
     @ManyToOne
     @JoinColumn(name = "user_id")
     // Schedule이 User와 연결되어야 한다.
     private User user;
 
+    public Schedule(){
+
+    }
+
+    public Schedule(String title, String contents){
+        this.title = title;
+        this.contents = contents;
+    }
+
+    public void updateContents(String contents){
+        this.contents = contents;
+    }
 }
