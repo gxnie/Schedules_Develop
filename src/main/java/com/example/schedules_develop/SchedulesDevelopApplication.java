bafdee289862c08fb1b8ac5a61d5c4ea6e317016
gxnie <wlsdl7520@naver.com>
2024-11-14 20:53:17 +0900

    init commit

---
+++ src/main/java/com/example/schedules_develop/SchedulesDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedules_develop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulesDevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulesDevelopApplication.class, args);
+    }
+
+}
