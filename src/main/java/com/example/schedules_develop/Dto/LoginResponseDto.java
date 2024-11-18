06cdd95242b891a8b322025fad3391b10b4bfc22
gxnie <wlsdl7520@naver.com>
2024-11-18 12:28:44 +0900

    feat: 로그인 응답 Dto 생성
    응답에 필요한 데이터 필드 id 생성

--- src/main/java/com/example/schedules_develop/Dto/LoginResponseDto.java
+++ src/main/java/com/example/schedules_develop/Dto/LoginResponseDto.java
@@ -1,4 +1,13 @@
 package com.example.schedules_develop.Dto;
 
+
+import lombok.Getter;
+
+@Getter
 public class LoginResponseDto {
+    private final Long id;
+
+    public LoginResponseDto(Long id){
+        this.id = id;
+    }
 }
