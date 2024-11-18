7735eff843c29e49d2fb6bb8d63a6ba5e9a3bd2d
gxnie <wlsdl7520@naver.com>
2024-11-18 12:28:18 +0900

    feat: 로그인 요청 Dto 생성
    로그인 요청시 필요한 이메일 비밀번호

--- src/main/java/com/example/schedules_develop/Dto/LoginRequestDto.java
+++ src/main/java/com/example/schedules_develop/Dto/LoginRequestDto.java
@@ -1,4 +1,19 @@
 package com.example.schedules_develop.Dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.antlr.v4.runtime.misc.NotNull;
+import jakarta.validation.constraints.NotBlank;
+
+@Getter
+@AllArgsConstructor
 public class LoginRequestDto {
+
+    @NotBlank
+    private final String email;
+
+    @NotNull
+    private final String password;
+
 }
